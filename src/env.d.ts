11ec69ad5f62e087710e60b33f10c6c51fa48c46
bdenham <brucedenham@mac.com>
2025-09-30 17:51:10 -0500

    Add TypeScript declaration for Speed Insights

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '@vercel/speed-insights/astro' {
+  const SpeedInsights: any;
+  export default SpeedInsights;
+}
